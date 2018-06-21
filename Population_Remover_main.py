d5d8582cd10abd540682abc8db90d6ba9db29f7e
Leo Choiniere <choiniel@leto13.iro.umontreal.ca>
2018-06-21 19:34:07 -0400

    Modified for better functionality

--- Population_Remover_main.py
+++ Population_Remover_main.py
@@ -1,11 +1,15 @@
 # coding=utf-8
+import time
 population="ACB" #Population to ignore in the out file 
 i=0
+list1=[]
+
+start_time =time.time()
 #We first extract ID of individuals in population
 with open("labels.txt") as f:
 	with open("output_pop.txt", "w") as f1:
-		f1.writelines(f.readlines())
-		next(f) #Skip header line
+		f1.writelines("")
+		#next(f) #Skip header line
 		for line in f:
 			if population in line:
 				list1.append(line.split(" ")[0])
@@ -13,13 +17,21 @@ with open("labels.txt") as f:
 			else:
 				f1.write(line)
 
-print("Number of %s found is %i ",format(population, i))
+print("Number of {} found is {} ".format(population, i))
 
 #Take the individuals on the list out of train.raw			
-with open("output_pop.txt") as f:
+with open("train.raw") as f:
 	with open("train_new.txt", "w") as f1:
-
+		j=0
 		for line in f:
+			i=0
+			j=j+1
 			for iteration in range(len(list1)):
-				if list1[iteration] in line:
-					f1.write(line)
+				if line.split(" ")[0] !=list1[iteration]:
+					i=i+1
+			print(j)
+			print(i)
+			print(len(list1))
+			if i==len(list1):
+				f1.write(line)
+print('--- {} seconds ----'.format(time.time()-start_time))
